1c0a53a1ceb67d594f288d2c6cabc4c99a40267d
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-09-19 16:20:26 +0300

    Initial button sample

---
+++ samples/button-sample.js
@@ -0,0 +1,33 @@
+var uijs = require('uijs');
+
+var box = uijs.box;
+var util = uijs.util;
+var positioning = uijs.positioning;
+var controls = require('..');
+var button = controls.button;
+var label = controls.label;
+var bind = uijs.bind;
+
+var app = box();
+
+var mybutton = app.add(button({
+  x: 10, y: 10, width: 300, height: 50,
+  background: '#a3e433',
+  color: 'black',
+  text: 'Yo',
+  image: null,
+  size: 20,
+}));
+
+app.add(label({
+  x: 10, y: 100, width: 300, height: 50,
+  color: 'black',
+  text: bind(function() { return mybutton.highlighted ? 'highlighted' : 'not highlighted'; }),
+  align: 'left',
+}));
+
+mybutton.on('click', function() {
+  // alert('hey, you clicked me you!');
+});
+
+module.exports = app;
